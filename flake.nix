26d456b6d611d03f4f2696a0083b5939fa813529
J <2742259+javdl@users.noreply.github.com>
2025-01-07 08:18:39 +0100

    fix some (but not all) licenses

--- flake.nix
+++ flake.nix
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2025 Joost van der Laan <joost@fashionunited.com>
+#
+# SPDX-License-Identifier: AGPL-3.0-only
+
 {
   description = "Development environment for top200-rs";
 
